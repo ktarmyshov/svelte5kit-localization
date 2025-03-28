e30f12e74fe842ee28620c7a4779b9b204dbdbd5
Konstantin Tarmyshov <k.tarmyshov@googlemail.com>
2025-03-28 18:37:36 +0100

    tests

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,7 +3,7 @@ import { extractLocales } from './localization/index.js';
 
 export async function load(event: LayoutServerLoadEvent) {
   const i18n = extractLocales(event);
-  console.log('i18n', JSON.stringify(i18n, null, 2));
+  // console.log('i18n', JSON.stringify(i18n, null, 2));
   return {
     i18n
   };
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -5,7 +5,7 @@ export async function load(event: LayoutLoadEvent) {
   // Get url path without tracking
   // Otherwise this will trigger on every navigation
   const urlpathname = event.untrack(() => event.url.pathname);
-  console.log('load: urlpathname', urlpathname);
+  // console.log('load: urlpathname', urlpathname);
   const i18n = await initialLoadLocalizations(urlpathname, {
     activeLocale: event.data.i18n.activeLocale
   });
